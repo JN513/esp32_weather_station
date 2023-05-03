f006ba046bb6c9e3334c15ec2f713b3e2ee19907
JN513 <jn5.guape@gmail.com>
2023-05-02 21:16:02 -0300

    biblkiotecas dos sensores

--- components/update/component.mk
+++ components/update/component.mk
@@ -1,3 +1,3 @@
 COMPONENT_ADD_INCLUDEDIRS = .
 
-COMPONENT_DEPENDS = esp_https_ota esp_http_client app_update time_manager esp-tls json spiffs
+COMPONENT_DEPENDS = esp_https_ota esp_http_client app_update esp-tls json spiffs
